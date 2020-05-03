e06f36886bbafe69e9d8939bf7fc41bc9b4809ca
submohanty <submohanty21@gmail.com>
2020-05-02 20:46:01 -0600

    Made a new budgeting / tax app that would actually show you a visual graph. This would be the first step in the process as this python dictionary gives you a list of the state income tax so users can enter in their salaries and get how much they would paying in income tax.

---
+++ pyFinance.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import math
+
+stateList = {"Alabama": 0.035, "Alaska": 0, "Arizona": 0.03565, "Arkansas": 0.039, "California": 0.0715,
+             "Colorado": 0.0463, "Connecticut": 0.04995, "Delaware": 0.044, "Florida": 0, "Georgia": 0.03375,
+             "Hawaii": 0.062, "Idaho": 0.04025, "Illinois": 0.0495, "Indiana": 0.0323, "Iowa": 0.0443, "Kansas": 0.044,
+             "Kentucky": 0.05, "Louisiana": 0.04, "Maine": 0.06475, "Maryland": 0.03875, "Massachusetts": 0.0505,
+             "Michigan": 0.0425, "Minnesota": 0.076, "Mississippi": 0.04, "Missouri": 0.037, "Montana": 0.0395,
+             "Nebraska": 0.0465, "Nevada": 0, "New Hampshire": 0.05, "New Jersey": 0.06075, "New Mexico": 0.033,
+             "New York": 0.0641, "North Dakota": 0.02, "Ohio": 0.034885, "Oklahoma": 0.0275, "Pennsylvania": 0.0307,
+             "Rhode Island": 0.0487, "South Carolina": 0.0405, "South Dakota": 0, "Tennessee": 0, "Texas": 0,
+             "Utah": 0.0495, "Vermont": 0.0615, "Virginia": 0.03875, "Washington": 0, "West Virginia": 0.0475,
+             "Wisconsin": 0.05825, "Wyoming": 0, "District of Columbia": 0.06475}
+
+
