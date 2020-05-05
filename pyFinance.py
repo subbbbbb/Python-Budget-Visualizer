3785beabf6bc454c0bb31c1a15f2a8fa1697929c
submohanty <submohanty21@gmail.com>
2020-05-05 11:11:11 -0600

    Brought the savings or debt to a separate function and to make it work made the variables used throughout global at the top of the code, and then made it global once again throughout main so then saveOrDebt could recognize the correct values for housing, transportation, etc.

--- pyFinance.py
+++ pyFinance.py
@@ -11,7 +11,15 @@ stateList = {"Alabama": 0.035, "Alaska": 0, "Arizona": 0.03565, "Arkansas": 0.03
              "Rhode Island": 0.0487, "South Carolina": 0.0405, "South Dakota": 0, "Tennessee": 0, "Texas": 0,
              "Utah": 0.0495, "Vermont": 0.0615, "Virginia": 0.03875, "Washington": 0, "West Virginia": 0.0475,
              "Wisconsin": 0.05825, "Wyoming": 0, "District of Columbia": 0.06475}
-
+global housing, transportation, bills, leisure, savings, debt, salary, tax
+housing = 0
+transportation = 0
+bills = 0
+leisure = 0
+savings = 0
+debt = 0
+salary = 0
+tax = 0
 
 def getTaxRate():
     state = input("Enter in the state that you live in")
@@ -34,6 +42,7 @@ def getTax(salary):
     return tax
 
 def main():
+    global housing, transportation, bills, leisure, savings, salary
     print("All numbers throughout this program are in US Dollars ($)")
     salary = getSalary()
     tax = getTax(salary)
@@ -47,15 +56,22 @@ def main():
         "fees/medications, etc.) "))
     leisure = float(input("Enter in how much you spend on any other \"fun\" things."))
     savings = (salary) - (housing + transportation + bills + leisure + tax)
+    if(savings < 0):
+        debt = ((salary) - (housing + transportation + bills + leisure + tax))
+        labels = "Housing", "Transportation", "Bills", "Leisure", "Debt","Tax"
+        sizes = [housing, transportation, bills, leisure, savings, tax]
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        ax1.axis('equal')
 
     labels = "Housing", "Transportation", "Bills", "Leisure", "Savings", "Tax"
     sizes = [housing, transportation, bills, leisure, savings, tax]
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
     ax1.axis('equal')
-
     plt.show()
-
+    saveOrDebt()
+def saveOrDebt():
     if (housing + transportation + bills + leisure + tax < salary):
         print("You are saving $" + str(savings) + " every month.")
     if (housing + transportation + bills + leisure + tax > salary):
